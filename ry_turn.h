aad3b15d35f9b7673afc361d2f5b221740fe6446
Russell Yanofsky <russ@yanofsky.org>
2001-11-12 15:51:14 +0000

    turn class

---
+++ ry_turn.h
@@ -0,0 +1,20 @@
+#ifndef ry_turn_h
+#define ry_turn_h
+
+
+class Turn
+{
+public:
+  int move;
+  Card card1, card2;
+  
+  Turn() : move(0), card1(), card2() { } 
+  
+  void describe()
+  {
+    
+  }
+};
+
+
+#endif
