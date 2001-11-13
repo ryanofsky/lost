b018760209a8443a59785eb9734e988882ef3c90
Russell Yanofsky <russ@yanofsky.org>
2001-11-13 17:42:20 +0000

    turn class declaration

--- ry_turn.h
+++ ry_turn.h
@@ -1,9 +1,7 @@
 #ifndef ry_turn_h
 #define ry_turn_h
 
-#include <iostream>
-using std::cerr;
-using std::cout;
+#include "ry_card.h"
 
 class Turn
 {
@@ -13,91 +11,9 @@ public:
   
   Turn() : move(0), card1(), card2() { } 
   
-  void output()
-  {
-    cout << "turn:";
-    int m = move & ~GameConstants::RANDOMDRAW;
-    if (m == GameConstants::PLAY)
-      cout << "campaign-home-";
-    else if (m == GameConstants::DROP)
-      cout << "discard-";
-    
-    if (m == GameConstants::PLAY || m == GameConstants::DROP)
-    {
-      cout << GameConstants::GetLongColor(card1.color) << ';'
-           << GameConstants::GetValue(card1.points) 
-           << GameConstants::GetShortColor(card1.color) << ';';
-      if ((move & GameConstants::RANDOMDRAW) != 0)
-        cout << "drawpile;";
-      else
-      {
-        cout << "discard-"
-             << GameConstants::GetLongColor(card2.color) << ';'
-             << GameConstants::GetValue(card2.points) 
-             << GameConstants::GetShortColor(card2.color) << ';';            
-      }  
-    }
-    else if (move == GameConstants::PASS)
-    {
-      cout << "pass;"
-           << GameConstants::GetValue(card1.points) 
-           << GameConstants::GetShortColor(card1.color) << ';'
-           << GameConstants::GetValue(card2.points) 
-           << GameConstants::GetShortColor(card2.color) << ';';
-    }
-    cout << endl;
-  }
+  void output();
+  void describe();
 
-  void describe()
-  {
-    int m = move & ~GameConstants::RANDOMDRAW;
-    if (m == GameConstants::PLAY)
-    {
-      cerr << "Played a ";
-      card1.describe();
-    }
-    else if (m == GameConstants::DROP)
-    {
-      cerr << "Discarded a ";
-      card1.describe();
-    };
-    
-    if (m == GameConstants::PLAY || m == GameConstants::DROP)
-    {
-      cerr << " and picked up a ";
-      if (card2)
-        card2.describe();
-      else
-        cerr << "card";
-      
-      if ((move & GameConstants::RANDOMDRAW) != 0)
-      {
-         cerr << " from the draw pile";
-      }
-      else
-      {
-        cerr << " from a discard stack";
-      }
-    }
-    else if (move == GameConstants::PASS)
-    {
-      if (!card1 && !card2) 
-      {
-        cerr << "Passed two cards";
-      }
-      else
-      {
-        cerr << "Passed a ";
-        card1.describe();
-        cerr << " and a ";
-        card2.describe();
-      }
-    }
-    else
-    {
-      cerr << "Unknown";
-    }
-  }
 };
 
 
