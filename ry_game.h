656972cdb7bb81df657e01def9ead99ef1b3da77
Russell Yanofsky <russ@yanofsky.org>
2001-11-13 17:35:32 +0000

    added counting heuristic

--- ry_game.h
+++ ry_game.h
@@ -3,6 +3,9 @@
 
 #include "ry_card.h"
 #include "ry_turn.h"
+#include <algorithm>
+
+using std::min;
 
 class Game
 {
@@ -13,6 +16,9 @@ public:
 
   void countCards()
   {
+    
+    // estimate by counting
+    
     emptyProbStacks(myhand);
     
     for(int i = 1; i < GameConstants::NUM_COLORS + 1; ++i)
@@ -22,6 +28,52 @@ public:
     {
       emptyProbStacks(campaigns[0][i]);
       emptyProbStacks(campaigns[1][i]);
+    }
+    
+    // estimate by looking at players last moves
+    
+    int players[] =
+    { 
+      GameConstants::HOMEP,
+      GameConstants::AWAY,
+      GameConstants::AWAYP
+    };
+    
+    for(int i = 0; i < 3; ++i)
+    {
+      int p = players[i];
+      Turn t = lastturns[p];
+      if (t.move & ~GameConstants::RANDOMDRAW == GameConstants::PLAY)
+      {
+        // unlikely they have any lower cards of the campaign they just played
+        // therefore lower probabilities of lower cards
+        changeProb(hands[p], t.card1.points, false, t.card1.color, false,2);
+        
+        // likewise, raise slightly the probability that they have higher cards
+        changeProb(hands[p], t.card1.points, true, t.card1.color, true,1.5);
+      }
+      else if (t.move & ~GameConstants::RANDOMDRAW == GameConstants::DROP)
+      {
+        // less likely they have any lower cards of the campaign they
+        // just dropped
+        changeProb(hands[p], t.card1.points, false, t.card1.color, false,0.5);
+        
+        // even less likely they have any higher cards of the campaign 
+        // they just dropped
+        changeProb(hands[p], t.card1.points, true, t.card1.color, false,1.0);
+      }
+    }
+    
+    Turn t = lastturns[GameConstants::HOMEP];
+    if (t.move == GameConstants::PASS)
+    {
+      if (t.card1.color == t.card2.color)
+      {
+        // if I was passed two cards of the same color, my teammate
+        // probably has higher cards in the same campaign
+        int v = min(t.card1.points, t.card2.points);
+        changeProb(hands[GameConstants::HOMEP], v, true, t.card1.color, true,2.0);
+      }
     }
   }  
 
@@ -72,6 +124,31 @@ private:
       drawpile.setprob(*it, 0);
       for(int i = 0; i < GameConstants::NUM_PLAYERS; ++i)
         hands[i].setprob(*it, 0);
+    }
+  }
+  
+  void inline changeProb(ProbStack & p, int startat, bool greater, int color, bool raise, double amt = 1.0)
+  {
+    int first, last, d;
+    
+    first = Card(color, startat).getIndex();
+    if (greater)
+      last = Card(color, GameConstants::CARD_MAX);
+    else  
+      last = Card(color, GameConstants::CARD_MIN) - 1;
+      
+    if (first < 0 || last < 0 || first == last) return;  
+    d = greater ? 1 : -1;
+    
+    for(;;)
+    {
+      first += d;
+      if (first == last) break;
+      double q = p.getprob(first);
+      if (raise)
+        p.setprob(first, 1.0 - (1.0-q) / (amt + 1.0));
+      else
+        p.setprob(first, q / (amt + 1.0));      
     }
   }
 };
