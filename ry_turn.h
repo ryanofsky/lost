147583bca2f0c79a3bb5cc1c5e98bd86a223bc4c
Russell Yanofsky <russ@yanofsky.org>
2001-11-14 20:20:55 +0000

    made turns comparable

--- ry_turn.h
+++ ry_turn.h
@@ -8,11 +8,17 @@ class Turn
 public:
   int move;
   Card card1, card2;
+  float eval;
   
-  Turn() : move(0), card1(), card2() { } 
+  Turn() : move(0), card1(), card2(), eval(0) { } 
   
   void output();
   void describe();
+  
+  bool operator<(Turn const & t)
+  {
+    return this->eval < t.eval;
+  }
 
 };
 
