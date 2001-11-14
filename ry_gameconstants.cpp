a3888767b8fa07a82663c23de0a6992e8f8886a8
Russell Yanofsky <russ@yanofsky.org>
2001-11-14 20:09:58 +0000

    documented and rearranged

--- ry_gameconstants.cpp
+++ ry_gameconstants.cpp
@@ -1,255 +1,122 @@
 #include "ry_gameconstants.h"
 #include "ry_card.h"
 
-GameConstants GC;
-extern GameConstants GC;
+// FAST & FRIENDLY GLOBAL VARIABLE
+const GameConstants GC;
+extern const GameConstants GC;
 
-const char * GameConstants::GetColor(int c)
+const GameConstants::CardInfo GameConstants::cardInfo[NUM_UCARDS] = 
 {
-  switch(c)
-  {
-    case GameConstants::RED:
-      return "Red";
-    break;
-    case GameConstants::YELLOW:
-      return "Yellow";
-    break;
-    case GameConstants::GREEN:
-      return "Green";
-    break;
-    case GameConstants::BLUE:
-      return "Blue";
-    break;
-    case GameConstants::WHITE:
-      return "White";
-    break;
-    default:
-     return "Unknown";
-    break;
-  }
-}
+  CardInfo( Card(RED,KICKER),    3 ),  // 0 
+  CardInfo( Card(RED,2),         2 ),  // 1 
+  CardInfo( Card(RED,3),         2 ),  // 2 
+  CardInfo( Card(RED,4),         2 ),  // 3 
+  CardInfo( Card(RED,5),         1 ),  // 4 
+  CardInfo( Card(RED,6),         1 ),  // 5 
+  CardInfo( Card(RED,7),         1 ),  // 6 
+  CardInfo( Card(RED,8),         1 ),  // 7 
+  CardInfo( Card(RED,9),         1 ),  // 8 
+  CardInfo( Card(RED,10),        1 ),  // 9 
+  CardInfo( Card(YELLOW,KICKER), 3 ),  // 10 
+  CardInfo( Card(YELLOW,2),      2 ),  // 11 
+  CardInfo( Card(YELLOW,3),      2 ),  // 12 
+  CardInfo( Card(YELLOW,4),      2 ),  // 13 
+  CardInfo( Card(YELLOW,5),      1 ),  // 14 
+  CardInfo( Card(YELLOW,6),      1 ),  // 15 
+  CardInfo( Card(YELLOW,7),      1 ),  // 16 
+  CardInfo( Card(YELLOW,8),      1 ),  // 17 
+  CardInfo( Card(YELLOW,9),      1 ),  // 18 
+  CardInfo( Card(YELLOW,10),     1 ),  // 19 
+  CardInfo( Card(GREEN,KICKER),  3 ),  // 20 
+  CardInfo( Card(GREEN,2),       2 ),  // 21 
+  CardInfo( Card(GREEN,3),       2 ),  // 22 
+  CardInfo( Card(GREEN,4),       2 ),  // 23 
+  CardInfo( Card(GREEN,5),       1 ),  // 24 
+  CardInfo( Card(GREEN,6),       1 ),  // 25 
+  CardInfo( Card(GREEN,7),       1 ),  // 26 
+  CardInfo( Card(GREEN,8),       1 ),  // 27 
+  CardInfo( Card(GREEN,9),       1 ),  // 28 
+  CardInfo( Card(GREEN,10),      1 ),  // 29 
+  CardInfo( Card(BLUE,KICKER),   3 ),  // 30 
+  CardInfo( Card(BLUE,2),        2 ),  // 31 
+  CardInfo( Card(BLUE,3),        2 ),  // 32 
+  CardInfo( Card(BLUE,4),        2 ),  // 33 
+  CardInfo( Card(BLUE,5),        1 ),  // 34 
+  CardInfo( Card(BLUE,6),        1 ),  // 35 
+  CardInfo( Card(BLUE,7),        1 ),  // 36 
+  CardInfo( Card(BLUE,8),        1 ),  // 37 
+  CardInfo( Card(BLUE,9),        1 ),  // 38 
+  CardInfo( Card(BLUE,1),        1 ),  // 39 
+  CardInfo( Card(WHITE,KICKER),  3 ),  // 40 
+  CardInfo( Card(WHITE,2),       2 ),  // 41 
+  CardInfo( Card(WHITE,3),       2 ),  // 42 
+  CardInfo( Card(WHITE,4),       2 ),  // 43 
+  CardInfo( Card(WHITE,5),       1 ),  // 44 
+  CardInfo( Card(WHITE,6),       1 ),  // 45 
+  CardInfo( Card(WHITE,7),       1 ),  // 46 
+  CardInfo( Card(WHITE,8),       1 ),  // 47 
+  CardInfo( Card(WHITE,9),       1 ),  // 48 
+  CardInfo( Card(WHITE,10),      1 ),  // 49 
+};
 
-const char * GameConstants::GetLongColor(int c)
+const GameConstants::ColorInfo GameConstants::colorInfo[NUM_COLORS] = 
 {
-  switch(c)
-  {
-    case GameConstants::RED:
-      return "red";
-    break;
-    case GameConstants::YELLOW:
-      return "yellow";
-    break;
-    case GameConstants::GREEN:
-      return "green";
-    break;
-    case GameConstants::BLUE:
-      return "blue";
-    break;
-    case GameConstants::WHITE:
-      return "white";
-    break;
-    default:
-      BARF("GameConstants::GetLongColor(int) was passed an invalid color.");
-    break;
-  }
-  return NULL;
-}
+  { 'R', "red"    },
+  { 'Y', "yellow" },
+  { 'G', "green"  },
+  { 'B', "blue "  },
+  { 'W', "white"  }
+};
 
-char GameConstants::GetShortColor(int c)
+const GameConstants::PlayerInfo GameConstants::playerInfo[NUM_PLAYERS] =
 {
-  switch(c)
-  {
-    case GameConstants::RED:
-      return 'R';
-    break;
-    case GameConstants::YELLOW:
-      return 'Y';
-    break;
-    case GameConstants::GREEN:
-      return 'G';
-    break;
-    case GameConstants::BLUE:
-      return 'B';
-    break;
-    case GameConstants::WHITE:
-      return 'W';
-    break;
-    default:
-      BARF("GameConstants::GetShortColor(int) was passed an invalid color.");
-    break;
-  }
-  return 0;
-}
+  { HOMETEAM, "home",  "My" },
+  { AWAYTEAM, "away",  "My Opponent's" },
+  { HOMETEAM, "homeP", "My Partner's"  },
+  { AWAYTEAM, "awayP", "My Opponent's Partner's" }
+};
 
-char GameConstants::GetValue(int v)
+char GameConstants::pointSymbol(int p)
 {
-  if (0 <= v && v <= 9)
-    return v + 0x30;
-  else if (v == GameConstants::KICKER)
+  if (0 <= p && p <= 9)
+    return p + 0x30;
+  else if (p == GameConstants::KICKER)
     return 'K';
-  else if (v == 10)
+  else if (p == 10)
     return 'T';
   else  
-    BARF("GameConstants::GetValue(int) was passed an invalid value.");
-  return 0;  
+    return 0;  
 }
 
-const char * GameConstants::GetTeam(int t)
+GameConstants::GameConstants()
 {
-  switch (t)
-  {
-    case GameConstants::HOME:
-      return "My";
-    break;
-    case GameConstants::AWAY:
-      return "My opponent's";
-    break;
-    case GameConstants::HOMEP:
-      return "My partner's";
-    break;
-    case GameConstants::AWAYP:
-      return "My opponent's partner's";
-    break;  
-    default:
-      return "Sombody's";
-    break;
-  }
-}
+  // initialize nonstatic lookup tables
+ 
+  cardChar[pointSymbol(KICKER)].points = KICKER;
+  for(int i = CARD_MIN; i < CARD_MAX; ++i)
+    cardChar[pointSymbol(i) - CARDCHAR_S].points = i;
+  for(int i = 0; i < NUM_COLORS; ++i)
+    cardChar[colorInfo[i].symbol - CARDCHAR_S].color = i;
 
-const Card GameConstants::cardcodes[] = 
-{
-  Card( NONE,  0 ),        // 0x30 '0'
-  Card( NONE,  0 ),        // 0x31 '1'
-  Card( NONE,  2 ),        // 0x32 '2'
-  Card( NONE,  3 ),        // 0x33 '3'
-  Card( NONE,  4 ),        // 0x34 '4'
-  Card( NONE,  5 ),        // 0x35 '5'
-  Card( NONE,  6 ),        // 0x36 '6'
-  Card( NONE,  7 ),        // 0x37 '7'
-  Card( NONE,  8 ),        // 0x38 '8'
-  Card( NONE,  9 ),        // 0x39 '9'
-  Card( NONE,  0 ),        // 0x3A ':'
-  Card( NONE,  0 ),        // 0x3B ';'
-  Card( NONE,  0 ),        // 0x3C '<'
-  Card( NONE,  0 ),        // 0x3D '='
-  Card( NONE,  0 ),        // 0x3E '>'
-  Card( NONE,  0 ),        // 0x3F '?'
-  Card( NONE,  0 ),        // 0x40 '@'
-  Card( NONE,  0 ),        // 0x41 'A'
-  Card( BLUE,  0 ),        // 0x42 'B'
-  Card( NONE,  0 ),        // 0x43 'C'
-  Card( NONE,  0 ),        // 0x44 'D'
-  Card( NONE,  0 ),        // 0x45 'E'
-  Card( NONE,  0 ),        // 0x46 'F'
-  Card( GREEN, 0 ),        // 0x47 'G'
-  Card( NONE,  0 ),        // 0x48 'H'
-  Card( NONE,  0 ),        // 0x49 'I'
-  Card( NONE,  0 ),        // 0x4A 'J'
-  Card( NONE,  KICKER ),   // 0x4B 'K'
-  Card( NONE,  0 ),        // 0x4C 'L'
-  Card( NONE,  0 ),        // 0x4D 'M'
-  Card( NONE,  0 ),        // 0x4E 'N'
-  Card( NONE,  0 ),        // 0x4F 'O'
-  Card( NONE,  0 ),        // 0x50 'P'
-  Card( NONE,  0 ),        // 0x51 'Q'
-  Card( RED,   0 ),        // 0x52 'R'
-  Card( NONE,  0 ),        // 0x53 'S'
-  Card( NONE, 10 ),        // 0x54 'T'
-  Card( NONE,  0 ),        // 0x55 'U'
-  Card( NONE,  0 ),        // 0x56 'V'
-  Card( WHITE, 0 ),        // 0x57 'W'
-  Card( NONE,  0 ),        // 0x58 'X'
-  Card( YELLOW,0 ),        // 0x59 'Y'
-  Card( NONE,  0 ),        // 0x5A 'Z'
-  Card( NONE,  0 ),        // 0x5B '['
-  Card( NONE,  0 ),        // 0x5C '\'
-  Card( NONE,  0 ),        // 0x5D ']'
-  Card( NONE,  0 ),        // 0x5E '^'
-  Card( NONE,  0 ),        // 0x5F '_'
-  Card( NONE,  0 ),        // 0x60 '`'
-  Card( NONE,  0 ),        // 0x61 'a'
-  Card( BLUE,  0 ),        // 0x62 'b'
-  Card( NONE,  0 ),        // 0x63 'c'
-  Card( NONE,  0 ),        // 0x64 'd'
-  Card( NONE,  0 ),        // 0x65 'e'
-  Card( NONE,  0 ),        // 0x66 'f'
-  Card( GREEN, 0 ),        // 0x67 'g'
-  Card( NONE,  0 ),        // 0x68 'h'
-  Card( NONE,  0 ),        // 0x69 'i'
-  Card( NONE,  0 ),        // 0x6A 'j'
-  Card( NONE,  KICKER ),   // 0x6B 'k'
-  Card( NONE,  0 ),        // 0x6C 'l'
-  Card( NONE,  0 ),        // 0x6D 'm'
-  Card( NONE,  0 ),        // 0x6E 'n'
-  Card( NONE,  0 ),        // 0x6F 'o'
-  Card( NONE,  0 ),        // 0x70 'p'
-  Card( NONE,  0 ),        // 0x71 'q'
-  Card( RED,   0 ),        // 0x72 'r'
-  Card( NONE,  0 ),        // 0x73 's'
-  Card( NONE, 10 ),        // 0x74 't'
-  Card( NONE,  0 ),        // 0x75 'u'
-  Card( NONE,  0 ),        // 0x76 'v'
-  Card( WHITE, 0 ),        // 0x77 'w'
-  Card( NONE,  0 ),        // 0x78 'x'
-  Card( YELLOW,0 ),        // 0x79 'y'
-  Card( NONE,  0 ),        // 0x7A 'z'
+  for(int i = 0; i < NUM_PLAYERS; ++i)
+    players[playerInfo[i].name] = i;
+
+  for(int i = 0; i < NUM_COLORS; ++i)
+    colors[colorInfo[i].name] = i;
+    
+  for(int i = 0; i < NUM_COLORS; ++i)
+    colors[colorInfo[i].name] = i;
+
+  tokens["hand"] = HAND;
+  tokens["discard"] = DISCARD;
+  tokens["campaign"] = CAMPAIGN;
+  tokens["lastturn"] = TURN;
+  tokens["drawpile"] = DRAWPILE;
+  tokens["handsize"] = HANDSIZE;
+    
+  moves["campaign"] = PLAY;
+  moves["discard"] = DROP;
+  moves["pass"] = PASS;
+  moves["drawpile"] = RANDOMDRAW;
+
 };
-
-const size_t GameConstants::cardcodes_s = 0x30;
-const size_t GameConstants::cardcodes_e = 0x30 + DIM(GameConstants::cardcodes);
-
-const CardInfo GameConstants::deckinfo[] = 
-{
-  CardInfo(Card(RED,    KICKER), 3),    // 0 
-  CardInfo(Card(RED,    2     ), 2),    // 1 
-  CardInfo(Card(RED,    3     ), 2),    // 2 
-  CardInfo(Card(RED,    4     ), 2),    // 3 
-  CardInfo(Card(RED,    5     ), 1),    // 4 
-  CardInfo(Card(RED,    6     ), 1),    // 5 
-  CardInfo(Card(RED,    7     ), 1),    // 6 
-  CardInfo(Card(RED,    8     ), 1),    // 7 
-  CardInfo(Card(RED,    9     ), 1),    // 8 
-  CardInfo(Card(RED,    10    ), 1),    // 9 
-  CardInfo(Card(YELLOW, KICKER), 3),    // 10 
-  CardInfo(Card(YELLOW, 2     ), 2),    // 11 
-  CardInfo(Card(YELLOW, 3     ), 2),    // 12 
-  CardInfo(Card(YELLOW, 4     ), 2),    // 13 
-  CardInfo(Card(YELLOW, 5     ), 1),    // 14 
-  CardInfo(Card(YELLOW, 6     ), 1),    // 15 
-  CardInfo(Card(YELLOW, 7     ), 1),    // 16 
-  CardInfo(Card(YELLOW, 8     ), 1),    // 17 
-  CardInfo(Card(YELLOW, 9     ), 1),    // 18 
-  CardInfo(Card(YELLOW, 10    ), 1),    // 19 
-  CardInfo(Card(GREEN,  KICKER), 3),    // 20 
-  CardInfo(Card(GREEN,  2     ), 2),    // 21 
-  CardInfo(Card(GREEN,  3     ), 2),    // 22 
-  CardInfo(Card(GREEN,  4     ), 2),    // 23 
-  CardInfo(Card(GREEN,  5     ), 1),    // 24 
-  CardInfo(Card(GREEN,  6     ), 1),    // 25 
-  CardInfo(Card(GREEN,  7     ), 1),    // 26 
-  CardInfo(Card(GREEN,  8     ), 1),    // 27 
-  CardInfo(Card(GREEN,  9     ), 1),    // 28 
-  CardInfo(Card(GREEN,  10    ), 1),    // 29 
-  CardInfo(Card(BLUE,   KICKER), 3),    // 30 
-  CardInfo(Card(BLUE,   2     ), 2),    // 31 
-  CardInfo(Card(BLUE,   3     ), 2),    // 32 
-  CardInfo(Card(BLUE,   4     ), 2),    // 33 
-  CardInfo(Card(BLUE,   5     ), 1),    // 34 
-  CardInfo(Card(BLUE,   6     ), 1),    // 35 
-  CardInfo(Card(BLUE,   7     ), 1),    // 36 
-  CardInfo(Card(BLUE,   8     ), 1),    // 37 
-  CardInfo(Card(BLUE,   9     ), 1),    // 38 
-  CardInfo(Card(BLUE,   10    ), 1),    // 39 
-  CardInfo(Card(WHITE,  KICKER), 3),    // 40 
-  CardInfo(Card(WHITE,  2     ), 2),    // 41 
-  CardInfo(Card(WHITE,  3     ), 2),    // 42 
-  CardInfo(Card(WHITE,  4     ), 2),    // 43 
-  CardInfo(Card(WHITE,  5     ), 1),    // 44 
-  CardInfo(Card(WHITE,  6     ), 1),    // 45 
-  CardInfo(Card(WHITE,  7     ), 1),    // 46 
-  CardInfo(Card(WHITE,  8     ), 1),    // 47 
-  CardInfo(Card(WHITE,  9     ), 1),    // 48 
-  CardInfo(Card(WHITE,  10    ), 1),    // 49 
-};
-
-const size_t GameConstants::deckinfo_size = DIM(GameConstants::deckinfo);
--- ry_gameconstants.h
+++ ry_gameconstants.h
@@ -1,72 +1,84 @@
 #ifndef ry_gameconstants_h
 #define ry_gameconstants_h
 
-#include <STDDEF.H>
+#include "ry_card.h"
+
 #include <map>
 #include <string>
 
 using std::map;
 using std::string;
-class Card;
-class CardInfo;
 
-class GameConstants // constants which are used all over the place
+class GameConstants
 {
-
 public:
 
-  enum { RED=0, YELLOW, GREEN, BLUE, WHITE }; // colors
-  enum { HAND = 0, CAMPAIGN, DISCARD, TURN, DRAWPILE, HANDSIZE }; // header tokens
-  enum { HOME = 0, AWAY, HOMEP, AWAYP }; // players
-  enum { PLAY = 1, DROP = 2, PASS = 3, RANDOMDRAW = 4 }; // move types, RANDOMDRAW is a flag
-  enum { KICKER = -1, NONE = -1 }; // other constants 
-  enum { CARD_MIN = 2, CARD_MAX = 10, NUM_COLORS = 5, NUM_PLAYERS = 4, NUM_CARDS = 75 }; // counts
+  // counts
+  enum { NUM_COLORS=5, NUM_PLAYERS=4, NUM_CARDS=75, NUM_UCARDS=50, NUM_TEAMS=2 };
+
+  // all purpose constant
+  enum { NONE = -1 }; 
+  
+  // colors 
+  enum { RED=0, YELLOW, GREEN, BLUE, WHITE };
+  
+  // players
+  enum { HOME = 0, AWAY, HOMEP, AWAYP };
+  
+  // teams
+  enum { HOMETEAM = 0, AWAYTEAM };  
+  
+  // card values
+  enum { KICKER=0, CARD_MIN=2, CARD_MAX=10 };
+  
+  // move types (RANDOMDRAW is treated like a flag)
+  enum { PLAY = 1, DROP = 2, PASS = 3, RANDOMDRAW = 4 };
+
+  // header tokens
+  enum { HAND = 0, CAMPAIGN, DISCARD, TURN, DRAWPILE, HANDSIZE };
+
+  // Lookup table for to find card values and frequencies using card indices
+  struct CardInfo
+  {
+    Card card;
+    short freq;
+    CardInfo(Card card_, short freq_) : card(card_), freq(freq_) { }
+  };
+  static const CardInfo cardInfo[NUM_UCARDS];
+
+  // Lookup table to find color names and symbols 
+  struct ColorInfo
+  {
+    char symbol;
+    char const * name;
+  };
+  static const ColorInfo colorInfo[NUM_COLORS];
+
+  // Lookup table to find player names
+  struct PlayerInfo
+  {
+    int team;
+    const char * name;
+    const char * possesive;
+  };
+  static const PlayerInfo playerInfo[NUM_PLAYERS];
+  
+  // Function to return point symbols
+  static char pointSymbol(int p);
+
+  // Lookup table to find card values using card symbols
+  enum { CARDCHAR_S = '0', CARDCHAR_E = 'Z' };
+  Card cardChar[CARDCHAR_E - CARDCHAR_S];
+
+  // More lookup tables
   typedef map<string,int> SymTable;
   SymTable colors;
   SymTable tokens;
   SymTable players;
   SymTable moves;
-  
-  static const Card cardcodes[];
-  static const size_t cardcodes_s;
-  static const size_t cardcodes_e;
-  static const CardInfo deckinfo[];
-  static const size_t deckinfo_size;
-  
-  GameConstants()
-  {
-    colors["blue"] = BLUE;
-    colors["green"] = GREEN;
-    colors["red"] = RED;
-    colors["white"] = WHITE;
-    colors["yellow"] = YELLOW;
-    
-    tokens["hand"] = HAND;
-    tokens["campaign"] = CAMPAIGN;
-    tokens["discard"] = DISCARD;
-    tokens["lastturn"] = TURN;
-    tokens["drawpile"] = DRAWPILE;
-    tokens["handsize"] = HANDSIZE;
-    
-    players["away"] = AWAY;
-    players["awayP"] = AWAYP;
-    players["home"] = HOME;
-    players["homeP"] = HOMEP;
-    
-    moves["campaign"] = PLAY;
-    moves["discard"] = DROP;
-    moves["drawpile"] = RANDOMDRAW;
-    moves["pass"] = PASS;
-    
-  }
-  
-  static const char * GetColor(int c);
-  static const char * GetTeam(int t);
-  static const char * GetLongColor(int c);
-  static char GetShortColor(int c);
-  static char GetValue(int v);  
+
+  // Constructor
+  GameConstants();
 };
-
-
 
 #endif
