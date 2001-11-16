0fc3eefa8ac95e3695b5ad41ac33b1ab537e411e
Russell Yanofsky <russ@yanofsky.org>
2001-11-16 00:32:44 +0000

    updated methods for changed structure

--- ry_probstacks.cpp
+++ ry_probstacks.cpp
@@ -1,20 +1,25 @@
 #include "ry_probstacks.h"
+#include "ry_help.h"
+
+#include <assert.h>
+#include<math.h>
 
 extern const GameConstants GC;
 
+using std::min;
+
+
 void ProbStacks::clear()
 {
-  for(int cardno = 0; i < GC.NUM_UCARDS; ++cardno)
+  for(int cardno = 0; cardno < GC.NUM_UCARDS; ++cardno)
   {
-    discardnum[cardno] = GC.NONE;
     for(int stack = 0; stack < NUM_STACKS; ++stack)
       prob[stack][cardno] = 0.0;
   }  
 
   for(int color = 0; color < GC.NUM_COLORS; ++color)
   {
-    if (!discardq[color].empty())
-      discardq[color] = priority_queue<DiscardNode>();
+    discardSize[color] = 0;
     for(int team = 0; team < GC.NUM_TEAMS; ++team)
       campaignmin[team][color] = GC.KICKER;
   }  
@@ -23,24 +28,45 @@ void ProbStacks::clear()
 void ProbStacks::addCard(int stack, int card)
 {  
   prob[stack][card] += 1.0;
-  if (stack == CAMPAIGN + GC.HOMETEAM || stack == CAMPAIGN + GC.AWAYTEAM)
+  ++stackisum[stack];
+  if (stack == GC.CAMPAIGN + GC.HOMETEAM || stack == GC.CAMPAIGN + GC.AWAYTEAM)
   {
     Card c = GC.cardInfo[card].card;
-    int team = stack - CAMPAIGN;
+    int team = stack - GC.CAMPAIGN;
     if (campaignmin[team][c.color] < c.points)
       campaignmin[team][c.color] = c.points;
   }
   else if (stack == DISCARD)
   {
-    int color = GC.cardInfo[card].color;
-    discardq[color].push(DiscardNode());
-    
-    discardtop[color] = card;
-    int & ds = discardsum[color];
-    discardnum[card] = ds++;
-    ++ds;
+    assert (GameConstants::NUM_CARDS % GameConstants::NUM_COLORS == 0);
+    int ncards = GameConstants::NUM_CARDS / GameConstants::NUM_COLORS;
+    int color = GC.cardInfo[card].card.color;
+    assert (discardSize[color] >= 0 && discardSize[color] < ncards);
+    int pos = ncards * color + discardSize[color] - 1;
+    discardOrder[pos] = card;
+    ++discardSize[color];    
   }
 }
+
+  int ProbStacks::discardTop(int color)
+  {
+    assert (GameConstants::NUM_CARDS % GameConstants::NUM_COLORS == 0);
+    if (discardSize[color] == 0)
+    {
+      return GameConstants::NONE;
+    }
+    else
+    {
+      return (color * (GameConstants::NUM_CARDS / GameConstants::NUM_COLORS) + discardSize[color]);    
+    }
+  }
+  
+  float ProbStacks::getProb(int stack, int card)
+  {
+    return prob[stack][card] * stackisum[stack] / stacksum[stack];
+  }
+
+
 
 void ProbStacks::setNumCards(int stack, int num)
 {
@@ -50,7 +76,7 @@ void ProbStacks::setNumCards(int stack, int num)
 void ProbStacks::normalize(const IsKnown isknown)
 {
   // total number of cards in the unknown stacks
-  double totalcards = 0;
+  float totalcards = 0;
   for(int stack = 0; stack < NUM_STACKS; ++stack)
   if (!isknown[stack])
   {
@@ -58,20 +84,20 @@ void ProbStacks::normalize(const IsKnown isknown)
     stacksum[stack] = 0;
   }
   
-  for(cardno = 0; cardno < GC.NUM_UCARDS; ++cardno)
+  for(int cardno = 0; cardno < GC.NUM_UCARDS; ++cardno)
   {    
     // actual sum of the probabilities for this card
-    double sum = 0;
+    float sum = 0;
     
     // ideal sum of the probabilities for this card
-    double isum = GC.cardInfo(c).freq;
+    float isum = GC.cardInfo[cardno].freq;
     
     for(int stack = 0; stack < NUM_STACKS; ++stack)
     if (isknown[stack])
       sum += prob[stack][cardno];
     
     // missing sum
-    double msum = isum - sum;
+    float msum = isum - sum;
     
     // divide up the missing sum amoung the different stacks
     for(int stack = 0; stack < NUM_STACKS; ++stack)
@@ -95,16 +121,16 @@ void ProbStacks::applySkew(ProbSkew & ps, IsKnown isknown)
   for(int cardno = 0; cardno < GC.NUM_UCARDS; ++cardno)
   {
     // actual sum of the probabilities for this card
-    double sum = 0;
+    float sum = 0;
     
     // ideal sum of the probabilities for this card
-    double isum = GC.cardInfo(c).freq;
+    float isum = GC.cardInfo[cardno].freq;
     
     for(int stack = 0; stack < NUM_STACKS; ++stack)
     {
-      double skew = pt.skew[stack][cardno];
-      double orig = prob[stack][cardno];
-      double & nval = prob[stack][cardno];
+      float skew = ps.skew[stack][cardno];
+      float orig = prob[stack][cardno];
+      float & nval = prob[stack][cardno];
       if (isknown[stack])
       {
         isum -= orig;
@@ -119,20 +145,72 @@ void ProbStacks::applySkew(ProbSkew & ps, IsKnown isknown)
       }  
     }
     
-    if (fequals(sum,0)) continue;
+    if (fequals(sum,(float)0.0)) continue;
     
     // normalization multiplier
-    double norm = isum / sum;
+    float norm = isum / sum;
     
     for(int stack = 0; stack < NUM_STACKS; ++stack)
     {
-      double & nval = prob[stack][cardno];
+      float & nval = prob[stack][cardno];
       if (!isknown[stack]) nval *= norm;
       stacksum[stack] += nval;
     }  
   }  
 }
 
+  void ProbStacks::passCard(int cardno, int source, int dest, bool fractional)
+  {
+    assert(source != dest);
+    --stackisum[source];
+    ++stackisum[dest];
+    float moveamt = 1.0;
+    if (cardno != GC.NONE)
+    {
+      moveamt = 0;
+      int color = GC.cardInfo[cardno].card.color;
+      int c = cardno;
+      int m = Card(color, GC.KICKER).getIndex();
+      do
+      {
+        float maxmove = 
+          min
+          (
+            (float) 1.0 - moveamt,
+            prob[source][c]
+          );
+      
+        prob[source][cardno] -= maxmove;
+        prob[dest][cardno] += maxmove;
+        moveamt += maxmove;
+        --c;
+        
+      } 
+      while(fractional && moveamt < 1.0 && c >= m); 
+      
+      if (source == DISCARD)
+      {
+        int ncards = GC.NUM_CARDS / GC.NUM_COLORS;
+
+        assert (discardSize[color] > 0);
+        --discardSize[color];
+        int pos = ncards * color + discardSize[color];
+        discardOrder[pos] = cardno;
+      }
+      else if (dest == DISCARD)
+      {
+        int ncards = GC.NUM_CARDS / GC.NUM_COLORS;
+        assert (discardSize[color] >= 0 && discardSize[color] < ncards);
+        int pos = ncards * color + discardSize[color];
+        discardOrder[pos] = cardno;
+        ++discardSize[color];
+      }
+    }
+    stacksum[source] -= moveamt;
+    stacksum[dest] += moveamt;  
+  }
+  
+  
 ProbSkew::ProbSkew()
 {
   for(int i = 0; i < ProbStacks::NUM_STACKS; ++i)
