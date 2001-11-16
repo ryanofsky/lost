88dab582a7dceca373ff1c7f653ceb1fd938b385
Russell Yanofsky <russ@yanofsky.org>
2001-11-16 10:45:39 +0000

    added summaries

--- ry_card.h
+++ ry_card.h
@@ -1,22 +1,34 @@
 #ifndef ry_card_h
 #define ry_card_h
 
+// Card class
 class Card
 {
 public:  
   
+  // set to GameConstants:: RED, YELLOW, GREEN, BLUE, WHITE, or NONE
   char color;
+  
+  // integer number of points, or GameConstants::KICKER
   char points;
   
+  // default constructor
   Card();
+  
+  // constructor with arguments
   Card(char color, char points);
 
+  // Set card c based on a two letter card code
+  static bool Parse(char c1, char c2, Card & c);
+
+  // Return the card's index in the GameConstants::cardInfo array
+  int getIndex() const;
+
+  // Print a description of the card to the standard error output
   void describe() const;
   
-  static bool Parse(char c1, char c2, Card & c);
-  int getIndex() const;
-  
-  operator bool();
+  // convenience method. if this->color is NONE returns false, else true
+  operator bool() const;
 };
 
 #endif
--- ry_turn.h
+++ ry_turn.h
@@ -3,23 +3,48 @@
 
 #include "ry_card.h"
 
+// Turn class, describes a turn made by (any) player
 class Turn
 {
 public:
+  // possible moves are PLAY, DROP, PASS, RANDOMDRAW (defined in gameconstants)
+  // randomdraw is treated like a flag. It can be ORed with DROP or PASS moves
+  // randomdraw is set when at the end of the turn, the player picks up from
+  // the drawpile instead of a discard pile
   int move;
+  
+  // If move is PASS, then card1 and card2 are set to the two cards that are
+  // being passed. If the cards passed are unknown, then the card1 and card2
+  // have color = NONE
+  
+  // If the move is PLAY or DROP, then card1 contains the card that was 
+  // played on a campaign, or dropped on a discard stack. card2 contains
+  // the card that was picked up at the end of the turn from either the
+  // drawpile, or a discard pile. If card2 is unknown, it has color = NONE
   Card card1, card2;
+
+  // used by the Game::FindSuccessors heuristic. Each potential move generated
+  // by that function is given a value that is higher when the move is perceived
+  // to be better for the current player
   float eval;
-  
-  Turn() : move(0), card1(), card2(), eval(0) { } 
-  
-  void output();
-  void describe();
-  
-  bool operator<(Turn const & t)
+
+  // comparison operator that compares moves based on their eval values.
+  // used when moves are stored in containers like Game::moves
+  bool operator<(Turn const & t) const
   {
     return this->eval < t.eval;
   }
 
+  // default constructor
+  Turn() : move(0), card1(), card2(), eval(0.0) { } 
+  
+  // print a description of the move that can be read by the master program
+  // to the standard output 
+  void output();
+  
+  // print a desciption of the move that is useful for debugging 
+  // to the standard error output
+  void describe();
 };
 
 
