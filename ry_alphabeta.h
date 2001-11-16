3fc30f0078a70c9f50dffb6f1f068d0696a8f1f0
Russell Yanofsky <russ@yanofsky.org>
2001-11-16 10:40:30 +0000

    merged the overloaded go methods

--- ry_alphabeta.h
+++ ry_alphabeta.h
@@ -1,23 +1,25 @@
 #ifndef ry_alphabeta_h
 #define ry_alphabeta_h
 
-#include "ry_gameconstants.h"
-#include "ry_card.h"
 #include "ry_turn.h"
 #include "ry_game.h"
 
+// Alpha Beta Search Class
 class ABsearch
 {
 public:
+  
+  // Constructor, maxdepth is the depth of the search tree
   ABsearch(int maxdepth) : _maxdepth(maxdepth)
   { }
   
-  Turn go(Game & game);
-  void go(Game & game, int depth, int whoseturn);
+  // Do alpha beta search on game. Last two arguments are used by the recursive function
+  void go(Game & game, int depth = 0, bool pbackedUp = false);
 
-private:
+protected:
+
+  // depth of the search tree (# of moves to look ahead before evaluating)
   int _maxdepth;
-
 };
 
 #endif
